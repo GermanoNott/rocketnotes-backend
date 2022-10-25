401a4ca60ae3b1d1a3a8d7dff31ffd5a1d9a1678
germano <germanottingham@hotmail.com>
2022-10-25 16:49:10 -0300

    install dotenv and pm2

--- src/server.js
+++ src/server.js
@@ -1,43 +1,44 @@
-require('express-async-errors')
+require('express-async-errors');
+require('dotenv/config');
 
-const migrationsRun = require('./database/sqlite/migrations')
+const migrationsRun = require('./database/sqlite/migrations');
 
-const AppError = require('./utils/AppError')
-const uploadConfig = require('./configs/upload')
+const AppError = require('./utils/AppError');
+const uploadConfig = require('./configs/upload');
 
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const routes = require('./routes/index.js')
+const routes = require('./routes/index.js');
 
-migrationsRun()
+migrationsRun();
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 //definir as requisições do Body vem em formato json
-app.use(express.json())
+app.use(express.json());
 
-app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 
-app.use(routes)
+app.use(routes);
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             status: 'error',
             message: error.message
-        })
+        });
     }
 
-    console.log(error)
+    console.log(error);
 
     return response.status(500).json({
         status: 'error',
         message: 'Internal server error'
-    })
-})
+    });
+});
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
